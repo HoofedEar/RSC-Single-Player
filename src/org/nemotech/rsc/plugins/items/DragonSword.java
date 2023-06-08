4a8505cdf593df3105b39b11c4f79b3da5dac3fd
HoofedEar <HoofedEar@users.noreply.github.com>
2023-06-07 20:19:05 -0700

    Fix message when trying to equip the Dragon Sword

--- src/org/nemotech/rsc/plugins/items/DragonSword.java
+++ src/org/nemotech/rsc/plugins/items/DragonSword.java
@@ -4,15 +4,18 @@ import org.nemotech.rsc.model.player.InvItem;
 import org.nemotech.rsc.model.player.Player;
 import org.nemotech.rsc.plugins.listeners.executive.WieldExecutiveListener;
 
-import static org.nemotech.rsc.plugins.Plugin.LOST_CITY;
-import static org.nemotech.rsc.plugins.Plugin.message;
+import static org.nemotech.rsc.plugins.Plugin.*;
 
+/*
+    Ensures that the player is allowed to wield the Dragon Sword
+ */
 @SuppressWarnings("unused")
 public class DragonSword implements WieldExecutiveListener {
     @Override
     public boolean blockWield(Player player, InvItem item) {
         if (item.getID() == 593 && player.getQuestStage(LOST_CITY) != 4) {
-            message(player,"You must complete the Lost City quest to wield this weapon.");
+            message(player,"you have not earned the right to wear this yet");
+            message(player,"you need to complete the Lost city of zanaris quest");
             return false;
         }
         return true;
